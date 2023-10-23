19e15ac93c33d87e01834c54ad54b60d003a02c8
Liaqat Paindah <liaqat.paindah@gmail.com>
2023-10-23 09:33:01 +0430

    Content of website updated

--- resources/views/approach.blade.php
+++ resources/views/approach.blade.php
@@ -12,60 +12,118 @@
 
   </div>
 </section>
-<section id="portfolio-details" class="portfolio-details ">
+    <section id="services3" class="services3 section-bg">
       <div class="container">
 
-        <div class="row gy-4">
-
-          <div class="col-lg-6">
-            <div class="portfolio-details-slider swiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-backface-hidden">
-              <div class="swiper-wrapper align-items-center" id="swiper-wrapper-e1095e36709130c46" aria-live="off" style="transform: translate3d(-1712px, 0px, 0px); transition-duration: 400ms;"><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-next" data-swiper-slide-index="2" role="group" aria-label="3 / 3" style="width: 856px;">
-                  <img src="assets/img/approach/portfolio-3.jpg" alt="">
-                </div>
-
-                <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="0" role="group" aria-label="1 / 3" style="width: 856px;">
-                  <img src="assets/img/approach/portfolio-1.jpg" alt="">
-                </div>
-
-
-                <div class="swiper-slide swiper-slide-next" data-swiper-slide-index="2" role="group" aria-label="3 / 3" style="width: 856px;">
-                  <img src="assets/img/approach/portfolio-3.jpg" alt="">
-                </div>
-
-              <div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-prev" data-swiper-slide-index="0" role="group" aria-label="1 / 3" style="width: 856px;">
-                  <img src="assets/img/approach/portfolio-1.jpg" alt="">
-                </div></div>
-              <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 2" aria-current="true"></span><span class="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 3"></span></div>
-            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
-          </div>
-
-          <div class="col-lg-6">
-            <div class="portfolio-description">
-              <h2>Business Growth </h2>
-              <p>
-                At MgtWell Consulting Services, we provide hands-on, bespoke Business Growth Consulting solutions to SMEs & start-ups. We help business organizations become well-oiled machinery reflecting confidence and professionalism essential to reaching their goals and vision. We strategically help benchmark businesses for operational and strategic gaps, create robust business management structures & methods and develop functional strategies and plans.
-              </p>
-            </div>
-            <div class="portfolio-description">
-              <h2>Coaching & Mentoring</h2>
-              <p>
-                At MgtWell Consulting Services, We provide mentoring & coaching support to business owners & company key staff to help them navigate business challenges.
-        In coaching & mentoring, we help businesses to see their blind spots and hold them responsible for their goals and visions as their accountability partner.
-              </p>
-            </div>
-          </div>
-
+        <div class="section-title" data-aos="fade-up">
+          <h2>Our approach to management consulting services</h2>
         </div>
         <div class="row">
-          <div class="section-title">
-            <h2>Our Approach </h2>
-          <p>
-            MgtWell Consulting Services approaches every client’s business as if it were our own. We believe a consulting firm should be more than an advisor. We put ourselves in our clients’ shoes, align our incentives with their objectives, and collaborate to unlock the full potential of their business. This builds deep and enjoyable relationships.
-            The right approach is necessary for the right outcome. MgtWell approaches work by applying its external knowledge to your organization’s internal way of doing work. We know that in order to maximize the potential of success for your organization we need to shape our expert advice in a way that applies to your way of doing business. This allows us to create rich relationships with our clients.
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-clipboard-data"></i></div>
+              <h4 class="title"><a href="">Assessment/Understanding</a></h4>
+              <p class="description">In this first stage, we examine the client’s current situation and conditions. This involves gaining an understanding of business organization’s dynamics, pinpointing opportunities, and threats/issues, and figuring out what actions can be taken to enhance the situation. The result of this stage is a preliminary plan that includes our suggestions.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-book-half"></i></div>
+              <h4 class="title"><a href="">Plan</a></h4>
+              <p class="description">Based on the assessment, we formulate a comprehensive plan to assist businesses in achieving their objectives. This plan is customized to SMEs and start-ups and the organization’s specific circumstances and is intended to address the opportunities and challenges identified during the assessment phase.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-clipboard-check"></i></div>
+              <h4 class="title"><a href="">Execution</a></h4>
+              <p class="description">We then proceed to put the plan into action. This involves collaborating closely with business owners to ensure that the plan is carried out effectively and efficiently.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-database-fill-down"></i></div>
+              <h4 class="title"><a href="">Coaching & Mentoring</a></h4>
+              <p class="description">At MgtWell Consulting Services, we provide mentoring & coaching support to business owners & company key staff to help them navigate business challenges. In coaching & mentoring, we help businesses to see their blind spots and hold them responsible for their goals and visions as their accountability partner.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-recycle"></i></div>
+              <h4 class="title"><a href="">Enhancement</a></h4>
+              <p class="description">After the project is finished, we concentrate on optimizing and continually improving the results of businesses. This involves analyzing the project’s outcomes, identifying areas for improvement, and making necessary modifications.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box">
+              <div class="icon"><i class="bi bi-graph-up-arrow"></i></div>
+              <h4 class="title"><a href="">Business Growth</a></h4>
+              <p class="description">At MgtWell Consulting Services, we provide hands-on, bespoke Business Growth Consulting solutions to SMEs & start-ups. We help business organizations become well-oiled machinery reflecting confidence and professionalism essential to reaching their goals and vision. We strategically help benchmark businesses for operational and strategic gaps, create robust business management structures & methods and develop functional strategies and plans.</p>
+            </div>
+          </div>
+          <p class="description">
+            Throughout this process, we employ a combination of qualitative and quantitative research techniques. We believe in blending robust scientific research methods with innovative and occasionally experimental applications. Our approach is rigorous, driven by methods, and based on evidence. For every new management project, we approach the problem with a fresh, unbiased mindset that suits each client’s unique situation.
           </p>
         </div>
-        </div>
-
       </div>
-    </section>
+    </section><!-- End Services Section -->
+
+    <section id="services3" class="services3 section-bg">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Our approach to monitoring and evaluation services</h2>
+          <p>
+            Our approach to monitoring and evaluation (M&E) services is systematic and comprehensive, designed to measure and assess the success and performance of projects, programs, or entire organizations. 
+          </p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box fit">
+              <div class="icon"><i class="bi bi-clipboard-data"></i></div>
+              <h4 class="title"><a href="">Theory of Change</a></h4>
+              <p class="description">We start by understanding the desired outcomes and how the intervention is expected to lead to those outcomes.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon-box fit">
+              <div class="icon"><i class="bi bi-book-half"></i></div>
+              <h4 class="title"><a href="">Performance Indicators</a></h4>
+              <p class="description"> We establish process (implementation) indicators and progression indicators to measure progress.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="icon-box fit">
+              <div class="icon"><i class="bi bi-database-fill-down"></i></div>
+              <h4 class="title"><a href="">Data Collection</a></h4>
+              <p class="description">We collect data on the characteristics of individual participants, enrolment and attendance, end of programme situation of beneficiaries, and costs of the programme.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box fit">
+              <div class="icon"><i class="bi bi-clipboard-data"></i></div>
+              <h4 class="title"><a href="">Monitoring</a></h4>
+              <p class="description"> This is a continuous process of collecting and analyzing information about a programme, comparing actual against planned results to judge how well the intervention is being implemented.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box fit">
+              <div class="icon"><i class="bi bi-recycle"></i></div>
+              <h4 class="title"><a href="">Evaluation</a></h4>
+              <p class="description">We systematically and objectively assess all elements of a programme (design, implementation, results achieved) to determine its overall worth or significance. This includes performance evaluations focusing on the quality-of-service delivery and the outcomes achieved by a programme.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6" data-aos="fade-up">
+            <div class="icon-box fit">
+              <div class="icon"><i class="bi bi-graph-up-arrow"></i></div>
+              <h4 class="title"><a href="">Cost-Benefit Analysis</a></h4>
+              <p class="description">We usually include information on the cost of the programme being monitored or evaluated. This allows us to judge the benefits of a programme against its costs and identify which intervention has the highest rate of return.</p>
+            </div>
+          </div>
+          <p class="description">
+            Throughout this process, we use a mix of tools, methods, and approaches including performance indicators, the logical framework approach, theory-based evaluation, formal surveys, rapid appraisal methods, participatory methods, surveys, cost-benefit and cost-effectiveness analysis, and impact evaluation. Our approach is rigorous, method-driven, evidence-based, and tailored to suit each client’s unique situation.
+          </p>
+        </div>
+      </div>
+    </section><!-- End Services Section -->
 @endsection
--- resources/views/team_profile.blade.php
+++ resources/views/team_profile.blade.php
@@ -16,12 +16,13 @@
     <div class="container">
         <div class="section-title">
             <h2>Our Team</h2>
-            <p>MgtWell Consulting Services is composed of a diverse team of over 150 experienced professionals,
-                including project managers, researchers, data analysts, and experts in monitoring and evaluation, and
-                communications. Our team has a wealth of experience and skills in their respective areas of expertise,
-                including research and survey, monitoring and evaluation, and management consulting. With years of
-                experience in their fields, our team is well-equipped to provide high-quality services and support to
-                our clients.</p>
+            <p>MgtWell Consulting Services prides itself on its exceptional team, which consists of more than 150 highly skilled professionals. This diverse group is comprised of project managers, business experts, mentors' trainers, researchers, data analysts, and specialists in monitoring and evaluation, as well as business management. Each member of our team brings a wealth of experience and expertise to the table. 
+            Our project managers possess a deep understanding of effective strategies and methodologies, ensuring that every project is executed with precision and efficiency. Our business experts have a keen eye for identifying opportunities and implementing innovative solutions that drive growth and success for our clients. The mentors' trainers on our team are dedicated to nurturing talent and empowering individuals to reach their full potential.
+            Furthermore, our researchers are skilled at conducting thorough and insightful studies, providing valuable insights that inform decision-making processes. The data analysts we have onboard possess exceptional analytical skills, enabling them to extract meaningful information from complex datasets. Our experts in monitoring and evaluation ensure that our clients' projects are constantly assessed and optimized for maximum impact.
+            With years of experience in their respective fields, our team members are well-equipped to deliver high-quality services and unwavering support to our clients.
+            Our professional team values the diverse needs of our clients and struggles to offer solutions and results that are personalized to the smallest detail and aligned with the highest quality standards.
+            .</p>
+            <br>
         </div>
         <div class="row">
             <div class="col-xl-4">
@@ -33,10 +34,6 @@
                         <h2>{{$team->name}}</h2>
                         <h3>{{$team->position}}</h3>
                         <div class="social-links mt-2">
-                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
                         </div>
                     </div>
                 </div>
