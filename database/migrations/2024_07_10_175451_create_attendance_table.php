f5086664d7e613d8be216895ae43e8f7c3158419
Liaqat-Paindah <liaqat.paindah@gmail.com>
2024-07-11 21:19:48 +0430

    migration, controller, models of HRMIS completed

---
+++ database/migrations/2024_07_10_175451_create_attendance_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained();
+            $table->date('date');
+            $table->time('clock_in_time')->nullable();
+            $table->time('clock_out_time')->nullable();
+            $table->enum('status', ['Present', 'Absent'])->default('Absent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance');
+    }
+}
